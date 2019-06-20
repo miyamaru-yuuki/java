4a6bc4e6520aa3f92c82987db2c45e8d8ef89ffd
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-20 18:31:05 +0900

    1831

--- src/Sample.java
+++ src/Sample.java
@@ -1,10 +1,10 @@
-
 public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int num = 10;
 		System.out.println("数値:" + num);
+		String str =System.Console().readLine();
 	}
 
 }
