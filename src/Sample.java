1f8e733d7ddbb6eafa4976e8694b80d50309fc7b
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-21 11:52:42 +0900

    1152

--- src/Sample.java
+++ src/Sample.java
@@ -2,9 +2,12 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int num = 10;
-		java.util.Scanner str = new java.util.Scanner(System.in);
-		String str2 = str.nextLine();
+		System.out.println("数を入力してください");
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		java.util.Scanner sc2 = new java.util.Scanner(System.in);
+		String str = sc.nextLine();
+		String str2 = sc2.nextLine();
+		int num = Integer.parseInt(str);
 		int num2 = Integer.parseInt(str2);
 		int num3 = num + num2;
 		System.out.println("数値:" + num3);
