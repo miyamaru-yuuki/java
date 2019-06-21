9bf9d20d494ebc76ef7287ea592bdfcaca26614a
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-21 10:22:24 +0900

    1022

--- src/Sample.java
+++ src/Sample.java
@@ -3,9 +3,11 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int num = 10;
-		System.out.println("数値:" + num);
 		java.util.Scanner str = new java.util.Scanner(System.in);
 		String str2 = str.nextLine();
+		int num2 = Integer.parseInt(str2);
+		int num3 = num + num2;
+		System.out.println("数値:" + num3);
 	}
 
 }
