01d789d0f31fc3f71ec35a8be53167bc6569b901
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-21 15:24:15 +0900

    1524

--- src/Sample.java
+++ src/Sample.java
@@ -2,14 +2,10 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("文字を入力してください");
-		java.util.Scanner sc = new java.util.Scanner(System.in);
-		String str = sc.nextLine();
+		int[] data = {5,7,9,2,8};
 
-		if(str.equals("abc")) {
-			System.out.println("OK");
-		}else {
-			System.out.println("NG");
+		for(int i=0;i<5;i++){
+			System.out.println(data[i]);
 		}
 	}
 }
