e4c65fe64497b71bbe4d440830cc0705fac90dfb
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-22 10:10:03 +0900

    1010

--- src/Sample.java
+++ src/Sample.java
@@ -1,11 +1,9 @@
 public class Sample {
-
+	static void test(){
+        System.out.println("test");
+        }
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int[] data = {5,7,9,2,8};
-
-		for(int num : data){
-			System.out.println(num);
-		}
+		test();
 	}
 }
