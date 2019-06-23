370c708993e0416a4863a3b1bd6a3bad0098d23d
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-23 12:24:30 +0900

    1224

--- src/Sample.java
+++ src/Sample.java
@@ -1,9 +1,10 @@
 public class Sample {
-	static void test(){
-        System.out.println("test");
+	static void test(int n){
+		n = n * 2;
+        System.out.println(n);
         }
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		test();
+		test(10);
 	}
 }
