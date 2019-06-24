49545a92753cc1663494b42d3b8266452de79726
miyamaruyuuki <ajptmjg@gmail.com>
2019-06-24 17:08:11 +0900

    1708

--- src/Sample.java
+++ src/Sample.java
@@ -1,11 +1,9 @@
 public class Sample {
-	static int test(int n){
-		n = n * 2;
-		return n;
-        }
+
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int ret = test(10);
-		System.out.println(ret);
+		Shouhin s = new Shouhin();
+		s.sname = "りんご";
+		s.show();
 	}
 }
---
+++ src/Shouhin.java
@@ -0,0 +1,10 @@
+
+public class Shouhin {
+	public int sid;
+	public String sname;
+	public int tanka;
+
+	void show(){
+	      System.out.println(sname);
+	}
+}
