e04f228315f70d80f233b749296fa4f93679c612
miyamaruyuuki <ajptmjg@gmail.com>
2020-07-27 14:16:39 +0900

    1416

--- src/Sample.java
+++ src/Sample.java
@@ -2,7 +2,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Shouhin s = new Shouhin(1,"りんご",200);
+		Shouhin s = new Shouhin(1,"りんご",300);
 		System.out.println(s.getSid());
 		System.out.println(s.getSname());
 		System.out.println(s.getTanka());
