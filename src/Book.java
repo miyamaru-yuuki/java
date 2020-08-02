0d0b5d03f74c5193448cd4be182a44361f362f2a
miyamaruyuuki <ajptmjg@gmail.com>
2020-08-02 21:29:04 +0900

    2128

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,13 @@
 class Book extends Shouhin{
 	private String author;
 
-//	public Book() {
-//		System.out.println("Book()");
-//	}
+	public Book() {
+		int sid = this.getSid();
+		String sname = this.getSname();
+		int tanka = this.getTanka();
+		super(sid,sname,tanka);
+		System.out.println("Book()");
+	}
 
 	public Book(int sid, String sname, int tanka,String author) {
 		super(sid,sname,tanka);
