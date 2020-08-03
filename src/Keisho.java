6973f24c442a379585d8306cf7a31c8805ea01e5
miyamaruyuuki <ajptmjg@gmail.com>
2020-08-04 07:18:36 +0900

    0718

--- src/Keisho.java
+++ src/Keisho.java
@@ -1,9 +1,12 @@
 public class Keisho {
 	public static void main(String[] args) {
-//		Book b = new Book(1,"草枕",500,"夏目漱石");
 //		b.show();
-		Shouhin s = new Shouhin(1,"りんご",300);
-		UriageCust u = new UriageCust(1,5,s,"田中");
-		u.show();
+		Book b = new Book();
+		b.show();
+		Shouhin s = new Book();
+		s.show();
+//		Shouhin s = new Shouhin(1,"りんご",300);
+//		UriageCust u = new UriageCust(1,5,s,"田中");
+//		u.show();
 	}
 }
