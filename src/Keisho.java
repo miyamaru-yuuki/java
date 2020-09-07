e90eb0d10bb7a0f26ab29e2e54ca613e0735a39b
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-07 13:23:59 +0900

    1323

--- src/Keisho.java
+++ src/Keisho.java
@@ -1,12 +1,8 @@
 public class Keisho {
 	public static void main(String[] args) {
-		Book b = new Book();
-		b.showAuthor();
-//		Shouhin s = new Book();
-//		s.showAuthor();
-//		UriageCust uc = new UriageCust(1,5,s,"田中");
-//		uc.show();
-//		Uriage u = new UriageCust(1,5,s,"田中");
-//		u.show();
+//		Book b = new Book();
+//		b.showAuthor();
+		Member m = new Member("田中",30);
+		m.show();
 	}
 }
