ea4aa55e65f91a9247ea3d9a00b54fd08efc712c
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-09 03:01:30 +0900

    0301

--- src/Keisho.java
+++ src/Keisho.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 
 public class Keisho {
 	public static void main(String[] args) {
-		Member m1 = new Member("田中",30);
-		Member m2 = new Member("山本",29);
-		Stuff s1 = new Stuff("山田",40,"会長");
-		Stuff s2 = new Stuff("佐藤",35,"副会長");
+//		Member m1 = new Member("田中",30);
+//		Member m2 = new Member("山本",29);
+//		Stuff s1 = new Stuff("山田",40,"会長");
+//		Stuff s2 = new Stuff("佐藤",35,"副会長");
 
 		ArrayList<Member> list = new ArrayList<Member>();
-		list.add(m1);
-		list.add(m2);
-		list.add(s1);
-		list.add(s2);
+		list.add(new Member("田中",30));
+		list.add(new Member("山本",29));
+		list.add(new Stuff("山田",40,"会長"));
+		list.add(new Stuff("佐藤",35,"副会長"));
 
 		for(Member m : list){
 			m.show();
