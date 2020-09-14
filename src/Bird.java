8e175d48cb889460a667eade088bffcb94e27cff
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-14 20:22:10 +0900

    2022

---
+++ src/Bird.java
@@ -0,0 +1,7 @@
+
+public class Bird extends Animal{
+
+	public Bird(String name) {
+		super(name);
+	}
+}
--- src/Chusho.java
+++ src/Chusho.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 
 public class Chusho {
 	public static void main(String[] args) {
-		
+
 		ArrayList<Animal> list = new ArrayList<Animal>();
 		list.add(new Dog("ポチ"));
 		list.add(new Cat("ミケ"));
+		list.add(new Bird("ピヨ"));
 
 		for(Animal a : list){
 			a.naku();
