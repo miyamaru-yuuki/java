810ee5f8b17678599f03e21cd0a00910525618ad
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-17 17:06:27 +0900

    1706

--- src/KuKuMondai.java
+++ src/KuKuMondai.java
@@ -1,34 +1,34 @@
-public class KuKuMondai extends Mondai{
-	private int num1;
-	private int num2;
-
-	public KuKuMondai() {
-		super();
-		this.num1 = new java.util.Random().nextInt(9) + 1;
-		this.num2 = new java.util.Random().nextInt(9) + 1;
-	}
-
-	void show(){
-		System.out.println(num1 +"✖️️"+ num2);
-	}
-
-	int getNum1() {
-		return num1;
-	}
-
-	int getNum2() {
-		return num2;
-	}
-
-	int getKotae() {
-		return num1*num2;
-	}
-
-	boolean check(int kotae) {
-		if(kotae == this.getKotae()) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-}
+//public class KuKuMondai extends Mondai{
+//	private int num1;
+//	private int num2;
+//
+//	public KuKuMondai() {
+//		super();
+//		this.num1 = new java.util.Random().nextInt(9) + 1;
+//		this.num2 = new java.util.Random().nextInt(9) + 1;
+//	}
+//
+//	void show(){
+//		System.out.println(num1 +"✖️️"+ num2);
+//	}
+//
+//	int getNum1() {
+//		return num1;
+//	}
+//
+//	int getNum2() {
+//		return num2;
+//	}
+//
+//	int getKotae() {
+//		return num1*num2;
+//	}
+//
+//	boolean check(int kotae) {
+//		if(kotae == this.getKotae()) {
+//			return true;
+//		}else {
+//			return false;
+//		}
+//	}
+//}
