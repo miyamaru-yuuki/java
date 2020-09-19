a2cc49f648886134adcb87a1e896d9b86998514e
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-19 14:12:56 +0900

    1412

--- src/KuKuMondai.java
+++ src/KuKuMondai.java
@@ -1,34 +1,10 @@
 public class KuKuMondai extends Mondai{
-	private int num1;
-	private int num2;
-
-	public KuKuMondai() {
-		super();
-		this.num1 = new java.util.Random().nextInt(9) + 1;
-		this.num2 = new java.util.Random().nextInt(9) + 1;
-	}
 
 	public void show(){
-		System.out.println(num1 +"✖️️"+ num2);
-	}
-
-	int getNum1() {
-		return num1;
-	}
-
-	int getNum2() {
-		return num2;
+		System.out.println(super.getNum1() +"✖️️"+ super.getNum2());
 	}
 
 	int getKotae() {
-		return num1*num2;
-	}
-
-	boolean check(int kotae) {
-		if(kotae == this.getKotae()) {
-			return true;
-		}else {
-			return false;
-		}
+		return super.getNum1()*super.getNum2();
 	}
 }
