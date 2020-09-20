bc6b34b7aaecb5b892a2774274a0306923983ef5
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-20 11:52:35 +0900

    1152

--- src/Chusho2.java
+++ src/Chusho2.java
@@ -3,7 +3,7 @@ public class Chusho2 {
 	public static void main(String[] args) {
 
 	Sankaku s = new Sankaku(10,5);
-	En e = new En(11);
+	En e = new En(10);
 
 	System.out.println(s.getMenseki());
 	System.out.println(e.getMenseki());
