fe0201a8f7863bc246cd7dfe3fb0144c383633f2
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-23 21:43:39 +0900

    2143

--- src/StrSample.java
+++ src/StrSample.java
@@ -3,11 +3,7 @@ public class StrSample {
 
 	public static void main(String[] args) {
 		String str = "1,りんご,100";
-		String[] tankaindex = str.split(",");
-		System.out.println(tankaindex[2]);
-
-//		for(String t  : tankaindex){
-//			System.out.println(t);
-//		}
+		String[] shouhinAry = str.split(",");
+		System.out.println(shouhinAry[2]);
 	}
 }
