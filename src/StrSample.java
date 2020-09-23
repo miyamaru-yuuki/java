f7f68bf054529b8f4286de42c33dc09df5b770b0
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-23 20:38:44 +0900

    2038

--- src/StrSample.java
+++ src/StrSample.java
@@ -2,8 +2,8 @@
 public class StrSample {
 
 	public static void main(String[] args) {
-		String str = "パイナップル,100";
-		int snameindex = str.indexOf(",");
-		System.out.println(str.substring(0,snameindex) );
+		String str = "1,りんご,100";
+		String[] tankaindex = str.split(",");
+		System.out.println(tankaindex);
 	}
 }
