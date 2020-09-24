f5a12fa8b798c21221414cd14ae994ed1e313702
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-24 15:12:04 +0900

    1512

--- src/StrSample.java
+++ src/StrSample.java
@@ -4,6 +4,7 @@ public class StrSample {
 	public static void main(String[] args) {
 		String str = "1,りんご,100";
 		String[] shouhinAry = str.split(",");
-		System.out.println(shouhinAry[2]);
+		int tankaDouble = Integer.parseInt(shouhinAry[2]) * 2;
+		System.out.println(tankaDouble);
 	}
 }
