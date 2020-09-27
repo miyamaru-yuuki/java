74ad7f781f6ff8251ff14fa263071870b05180c9
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-27 16:28:03 +0900

    1627

--- src/Reigai.java
+++ src/Reigai.java
@@ -2,7 +2,8 @@ public class Reigai {
 	public static void main(String[] args) {
 		try{
 		    // 通常処理
-			int num = 10 / 0 ;
+			String num = new java.util.Scanner(System.in).nextLine();
+			int ans = 10 / Integer.parseInt(num);
 		}catch(ArithmeticException e){
 		    // エラー処理（エラーメッセージ名を出すなど）
 			System.out.println("０で除算しました");
