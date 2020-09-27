da2a11b8d6c4af64a3005708fc1be57d6f2026bd
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-27 21:53:50 +0900

    2153

--- src/Reigai.java
+++ src/Reigai.java
@@ -1,13 +1,18 @@
+import java.io.FileWriter;
+
 public class Reigai {
 	public static void main(String[] args) {
-		try {
+//		try {
 			func();
-		}catch(ArithmeticException e){
-		    // エラー処理（エラーメッセージ名を出すなど）
-			System.out.println("０で除算しました");
-		}catch(NumberFormatException e){
-			System.out.println("数値を入力してください");
-		}
+//		}catch(ArithmeticException e){
+//		    // エラー処理（エラーメッセージ名を出すなど）
+//			System.out.println("０で除算しました");
+//		}catch(NumberFormatException e){
+//			System.out.println("数値を入力してください");
+//		}
+			FileWriter fw = new FileWriter("test.txt");
+			fw.write("abc");
+			fw.close();
 	}
 	public static void func() {
 		// 通常処理
