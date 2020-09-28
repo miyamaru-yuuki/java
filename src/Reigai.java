46f14ef8323cd0f69859daa24edbd5e8c49dfb01
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-28 19:21:05 +0900

    1921

--- src/Reigai.java
+++ src/Reigai.java
@@ -8,8 +8,10 @@ public class Reigai {
 		}catch(ArithmeticException e){
 		    // エラー処理（エラーメッセージ名を出すなど）
 			System.out.println("０で除算しました");
+			func();
 		}catch(NumberFormatException e){
 			System.out.println("数値を入力してください");
+			func();
 		}
 		func2();
 	}
@@ -17,6 +19,7 @@ public class Reigai {
 	public static void func() {
 		String num = new java.util.Scanner(System.in).nextLine();
 		int ans = 10 / Integer.parseInt(num);
+		System.out.println(ans);
 	}
 
 	public static void func2() throws IOException {
