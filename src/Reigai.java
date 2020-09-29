36c5a712ce1d960a6598007526e5f5abf0e235f4
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-29 17:58:01 +0900

    1757

--- src/Reigai.java
+++ src/Reigai.java
@@ -17,8 +17,10 @@ public class Reigai {
 			}catch(ArithmeticException e){
 			    // エラー処理（エラーメッセージ名を出すなど）
 				System.out.println("０で除算しました");
+				System.out.println(e.getMessage());
 			}catch(NumberFormatException e){
 				System.out.println("数値を入力してください");
+				System.out.println(e.getMessage());
 			}
 		}
 	}
